c147b40480c614ee66e0bde7ff0e44a679c41a5e
DiganmeGiovanni <giovanni.fi05@gmail.com>
2014-01-07 17:15:55 -0600

    Agregadas funciones para analizar carpetas aplicando filtros y analicis recursivo

--- src/js/readingFiles.js
+++ src/js/readingFiles.js
@@ -11,8 +11,10 @@ WL.init({
 });
 
 // Muestra archivos y folders de una carpeta de skydrive
-// en caso de que el parametro sea null, mostrara los de la raiz
-function listarArchivos(folderID) 
+// en caso de que el parametro folderID sea null, mostrara los de la raiz
+// en caso de que el parametro filtro sea null mostrara todos los archivos.
+// los filtros validos son: 'audio', 'folder'
+function listarArchivos(folderID, filtro)
 {
     var files_path = folderID;
     if (folderID === null)
@@ -24,21 +26,36 @@ function listarArchivos(folderID)
         path: files_path,
         method: "GET"
     }).then(
-        function (response) {
+        function (response) 
+        {
             var archivos = [];
-            for (var i = 0; i < response.data.length; i++) 
+            if (filtro === null) // Listar archivos sin filtros
             {
-                archivos.push(response.data[i].type + " ---> " + response.data[i].name);
+                for (var i = 0; i < response.data.length; i++) 
+                {
+                    archivos.push(response.data[i].type + " ---> " + response.data[i].name);
+                }
+            }
+            else // Listar archivos utilizando un filtro
+            {
+                for (var i = 0; i < response.data.length; i++) 
+                {
+                    if (response.data[i].type === filtro) 
+                    {
+                        archivos.push(response.data[i].type + " ---> " + response.data[i].name);
+                    }
+                }
             }
             mostrarLista(archivos);
         },
-        function (response) {
+        function (response) 
+        {
             console.log("Ocurrio un error al tratar de acceder a los datos");
         }
     )
 }
 
-// Muestra todos los folders de un usuario
+// Muestra los folders principales de un usuario
 function listarFolders()
 {
     var files_path = "me/skydrive/files";
@@ -69,12 +86,107 @@ function listarFolders()
 function listarUnFolder()
 {
     WL.fileDialog({
-        mode: 'open',
+        mode: 'save',
+        select: 'single'
+    }).then(
+        function (response) 
+        {
+            var selectedFolder = response.data.folders[0].id;
+            var pathFolder = selectedFolder + "/files";
+            console.log("INSPECCIONANDO: " + pathFolder);
+            listarArchivos(pathFolder, null);
+        },
+        function (response) 
+        {
+            console.log("Un error ocurrio con el file-picker");
+        }
+    )
+}
+
+// Analiza un folder y agrega los archivos encontrados al arreglo recivido
+// en el parametro 'files', si el parametro filtro es igual a null, se
+// agregaran todos los archivos encontrados al arreglo. si el parametro
+// recursivo es igual a true, se analizara cada subfolder encontrado.
+function encolarArchivos(filtro, recursividad, folderPath, files)
+{
+    WL.api({
+        path: folderPath,
+        method: "GET"
+    }).then(
+        function (response) 
+        {
+            //var files = [];
+            files.push(" ");
+            files.push("FOLDER: ------------------")
+            for (var i = 0; i < response.data.length; i++) 
+            {
+                if (recursividad === true && response.data[i].type === "folder") 
+                {
+                    encolarArchivos(filtro, recursividad, response.data[i].id + "/files", files);
+                    //files.concat(folderFiles);
+                };
+                if (filtro === null)
+                {
+                    files.push(response.data[i].type + " --> " + response.data[i].name)
+                }
+                else if (response.data[i].type === filtro)
+                {
+                    files.push(response.data[i].type + " --> " + response.data[i].name);
+                    console.log("Agregado --> " + response.data[i].name);
+                }
+            }
+            console.log("EL ARREGLO CONTIENE: " + files.length + " elementos");
+            mostrarLista(files);
+        },
+        function (error_response) 
+        {
+            console.log("Ocurrio un error al tratar de crear la lista de archivos");
+        }
+    );
+}
+
+// Muestra un file-picker de skydrive, permite seleccionar un forlder
+// y lista los archivos de audio encontrados en este.
+function listarCanciones() 
+{
+    WL.fileDialog({
+        mode: 'save',
         select: 'single'
     }).then(
         function (response) {
-            var selectedFolder = response.data.files[0].id;
-            var pathFolder = "/me/skydrive.files/" + selectedFolder;
+            var selectedFolder = response.data.folders[0].id + "/files";
+            listarArchivos(selectedFolder, "audio");
+        },
+        function (error_response) 
+        {
+            console.log("Ocurrio un error al seleccionar un folder");
+        }
+    )
+}
+
+// Muestra un file-picker de skydrive, permite seleccionar un folder
+// y lista los archivos de audio encontrados en el interior, analiza tambien
+// subfolders de cada folder encontrado.
+function buscarAudioRecursivo() 
+{
+    WL.fileDialog
+    ({
+        mode: 'save',
+        select: 'single'
+    }).then
+    (
+        function (response) 
+        {
+            var selectedFolder = response.data.folders[0].id + "/files";
+            var files = [];
+            encolarArchivos("audio", true, selectedFolder, files);
+            console.log("============= SE DEVOLVIO ===========");
+            console.log(files);
+            mostrarLista(files);
+        },
+        function (error_response) 
+        {
+            console.log("Ocurrio un error al seleccionar un folder");
         }
     )
 }
@@ -82,6 +194,8 @@ function listarUnFolder()
 // Muestra en la pagina una lista de datos como respuesta para el usuario.
 function mostrarLista(lista) 
 {
+    $('#info').empty();
+    console.log("Se listaran: " + lista.length + " datos del arreglo.");
     for (var i = 0; i < lista.length; i++) 
     {
         $('#info').append("<span>" + lista[i] + "</span><br />");
